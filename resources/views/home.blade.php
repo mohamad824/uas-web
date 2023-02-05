14c3dbc5d4c33f2d0b586a90e25c541aaf6bfaad
mohamad824 <julpi498@gmail.com>
2023-02-05 21:33:06 +0700

    pertama commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,293 @@
+<!<!DOCTYPE html>
+
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>home</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Raleway&display=swap"
+            rel="stylesheet">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="tailwind.config.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+    </head>
+
+    <body class="bg-black">
+        <header class="bg-header">
+            <section id="navigation" class=" flex container mx-auto py-5 gap-5 justify-between items-center">
+                <div id="left-side" class="flex gap-10 items-center">
+                    <img src="asset/Group 1.svg" alt="">
+                    <nav>
+                        <ul class="flex gap-10 items-center text-white">
+                            <img src="logo/rumah.png" alt="">
+                            <li><a href="#">
+                                    <h4>MaemMama</h4>
+                                </a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#" class="text-[#FF9213]">Home</a></li>
+                            <li><a href="#">Menu</a></li>
+                            <li><a href="#">Community</a></li>
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div id="right-side" class="flex items-center gap-x-[40px]">
+                    <button><img src="logo/bg-makan.png" alt=""></button>
+                    <button class="text-white">
+                        <a href="/login" class="nav-link"><i class="bi bi-box-arrow-in-left"></i>
+                            Login</a>
+                    </button>
+                    <button>
+                        <img src="logo/bg org.png" alt="">
+                    </button>
+                    <a href="#" class="bg-[#FF9900] rounded-full  px-[15px] py-[10px] text-white">Registration</a>
+                </div>
+            </section>
+
+            <section id="hero" class="mt-[80px] text-center img-center ">
+
+
+                <div class="max-w-[650px] mx-auto items-center">
+                    <img src="img/Frame 2073.png" alt="">
+                    <h1 class="text-5xl font-cabinet grotesk font-bold text-left text-white">Makes You Satisfied With
+                        What You <span style="color:orange ;"> Ordered</span></h1>
+
+
+                    <div class="action flex justify-center items-center gap-x-4 mb-4">
+                        <div class="items-center">
+
+                            <h1 class="text-white "><STRong> Hamburger
+                                </STRong></h1>
+                            <h1 class="text-white"><strong> Best in The Universe</strong></h1>
+                            <br>
+                            <p class="text-left 150px text-white mb-10 ">Lörem ipsum ir dons det pren bokstav. Presav
+                                velavis dirar.
+                                Semidonde pregon. Terasåse seligt samt intrant. </p>
+                            <a href="#" class="border bg-[#FF9900] rounded-full  text-white">Order Now</a>
+                            <a href="#" class="border  rounded-full text-white">View Menu -></a>
+                            <a href="#"></a>
+
+                        </div>
+
+
+                        <div>
+
+
+
+                            <img src="img/image1 (1).png" class="mx-auto " alt="">
+                        </div>
+
+                    </div>
+                </div>
+
+            </section>
+
+            <section id :"footer">
+
+                <div>
+                    <h1 class=" text-[18px] text-[#646464] text-center my-[40px] font-Raleway font-semibold ">Trusted
+                        Companies Woldwide</h1>
+                    <nav id="sponsor">
+                        <ul class="flex gap-5 justify-center mb-[59px] gap-x-[60px]">
+                            <li><img src="logo/Vector (1).png" alt=""></li>
+                            <li><img src="logo/path30.png" alt=""></li>
+                            <li><img src="logo/path2.png" alt=""></li>
+                            <li><img src="logo/path2 (1).png" alt=""></li>
+                            <li><img src="logo/path30.png" alt=""></li>
+                            <li><img src="logo/path2.png" alt=""></li>
+                        </ul>
+                    </nav>
+                </div>
+            </section>
+            <section>
+                <div class="action flex justify-center items-center gap-x-4 mb-4">
+
+                    <p class="text-left 75px text-white mb-10 ">
+                        <a href="" class="text-[#FF9213]">About Us</a> <br>
+                        <strong>MaemMamam</strong> <br>
+                        We were founded in 1948 after the war and the world began to be safe we decided <br>
+                        to open a small restaurant on the side of the road for the youth and the surrounding <br>
+                        community, until finally we had the capital to create the only largest restaurant and <br>
+                        so far we have opened 10 branches in Indonesia.
+                        Read more <br>
+                        <a href="" class="border  rounded-full  text-white">View Menu -></a>
+                    </p>
+
+
+                    <a href="#"></a>
+                    <div>
+
+                        <a href="#" class="flex items-center gap-x-3 underline width 200px">
+                            <img src="img/image1 (2).png" class="mx-auto " alt="">
+                        </a>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div class="action flex justify-center items-center gap-x-4 mb-4">
+                    <div class="lift-items"><a href=""><img src="img/image1 (3).png" alt=""></a></div>
+                    <p class="text-center 80px text-white mb-15">
+                        <a class="text-[#FF9213]">MaemMamam Restaurant</a><br> <STRong> Fast food and fill your stomach
+                        </STRong><br>
+                        we are a fast food company and we make everyone enjoy any food we serve them <br>
+                        because we serve the best for them all. <br>
+                        <a href="" class="bg-[#FF9900] rounded-full text-white">Order Now</a>
+                        <a href="" class="border rounded-full  text-white">View Menu -></a>
+                        <a href=""></a>
+                    </p>
+
+                </div>
+
+            </section>
+            <section>
+                <div class="flex justify-center pt-[78px] gap-x-[187px]">
+                    <div class="relative"><img src="asset/Frame 1.svg" alt=""></div>
+                    <div><img src="asset/Frame 2.svg" alt=""></div>
+                    <div><img src="asset/Frame 3.svg" alt=""></div>
+                    <div><img src="asset/Frame 4.png" alt=""></div>
+                </div>
+                <div class="flex justify-left pt-[15px] gap-x-[15px]">
+                    <p class="text-left 100px text-white mb-15 ">
+                        <a href="" class="text-[#FF9213]">Popular menu</a> <br>
+                        <strong>Best Popular Menu</strong> <br>
+                        Find the food you want to buy and get the prize, made from the best ingredients we <br> choose
+                        <br>
+                    </p>
+
+                </div>
+
+                <div class="flex justify-center pt-[15px] gap-x-[15px]">
+                    <div class="max-w-[240px]">
+                        <h1 class="font-bold"></h1>
+                        <img src="logo/Frame 2074 (4).png" alt="">
+                    </div>
+                    <div class="max-w-[240px] ">
+                        <h1 class="font-bold"></h1>
+                        <img src="logo/Frame 2074 (1).png" alt="">
+                    </div>
+                    <div class="max-w-[240px] ">
+                        <h1 class="font-bold"></h1>
+                        <img src="logo/Frame 2074 (2).png" alt="">
+                    </div>
+                    <div class="max-w-[240px] ">
+                        <h1 class="font-bold"></h1>
+                        <img src="logo/Frame 2074 (7).png" alt="">
+                    </div>
+                </div>
+                <div class="flex justify-center pt-[20px] gap-x-[20px]">
+                    <div class="max-w-[240px]">
+                        <h1 class="font-bold"></h1>
+                        <img src="logo/Frame 2074 (4).png" alt="">
+                    </div>
+                    <div class="max-w-[240px] ">
+                        <h1 class="font-bold"></h1>
+                        <img src="logo/Frame 2074 (1).png" alt="">
+                    </div>
+                    <div class="max-w-[240px] ">
+                        <h1 class="font-bold"></h1>
+                        <img src="logo/Frame 2074 (2).png" alt="">
+                    </div>
+                    <div class="max-w-[240px] ">
+                        <h1 class="font-bold"></h1>
+                        <img src="logo/Frame 2074 (7).png" alt="">
+                    </div>
+                </div>
+                <div><br></div>
+                <div class="border   rounded-full  items-center">
+                    <p class="text-[#FF9213] text-center">Order Now</p>
+                    <h1 class="text-white text-center "><strong> Order Now And Take a Giv away</strong></h1>
+                    <br>
+                    <p class="text-center 75px text-white mb-10 ">
+                        Order and get the prize, we will give a gift to those of you who are lucky to get something <br>
+                        interesting from us <br>
+                        <a href="" class="bg-[#FF9900] rounded-full  text-white text-center ">Order Now</a>
+                    </p>
+
+                </div>
+            </section>
+            <footer>
+                <section id="Footer" class="flex container mx-auto py-5 gap-5 justify-center items-center">
+                    <div class="flex gap-10 items-center pl-[120px] pr-[120px] pb-[209px]">
+                        <div class="pt-[90px]">
+                            <img src="logo/rumah.png" alt="">
+                            <p class="text-left text-[20px] pt-[40px] text-[#969696]">We were founded in 1948 after the
+                                war and the world began to be safe we decided to open a small restaurant on the side of
+                                the road for the youth and the surrounding community.</p>
+                            <ul class="flex gap-x-[24px] pt-[40px]">
+                                <li>
+                                    <a><img src="logo/fb.png" alt=""></a>
+
+                                </li>
+                                <li>
+                                    <a><img src="logo/tw.png" alt=""></a>
+                                </li>
+                                <li>
+                                    <a><img src="logo/in.png" alt=""></a>
+                                </li>
+                                <li>
+                                    <a><img src="logo/ig.png" alt=""></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="pl-[24px] pt-[64px]">
+                            <h1 class=" text-[16px] font-bold text-white">Navigation</h1>
+                            <ul class="pt-[24px] text-[14px] leading-[24px] text-white">
+                                <li><a href=""> Home</a></li>
+                                <li><a href=""> Info</a></li>
+                                <li><a href=""> News</a></li>
+                                <li><a href=""> About As</a></li>
+                                <li><a href=""> Contact</a></li>
+                            </ul>
+                        </div>
+                        <div class="pl-[40px] pt-[20px] text-white">
+                            <h1 class=" text-[16px] font-bold text-left">Address</h1>
+                            <ul class="pt-[24px] text-[14px] w-[268px] leading-[24px]">
+                                <li><a href="">Jl. Al-Muwahhiddin, Karangtengah,
+                                    </a></li>
+                                <li><a href=""> Cibadak, Kab. Sukabumi.
+                                    </a></li>
+                                <li><a href=""> Sukabumi 43351
+                                    </a></li>
+
+                            </ul>
+                        </div>
+                        <div class="text-white">
+                            <h1 class="font-bold text-[16px] text-left pt-[40px]">Contact</h1>
+                            <ul class="pt-[24px] text-[14px]">
+                                <li class="w-[200px] leading-[16px] pt-[24px]">
+                                    <button>
+                                        <img src="logo/hp.png" alt=" ">
+                                    </button>
+                                    <a href="">083106403314</a>
+                                </li>
+                                <li class="w-[173px] leading-[16px] pt-[16px]">
+                                    <button>
+                                        <img src="logo/email.png" alt=" ">
+                                    </button>
+                                    <a href="">Rockjaiki@gmail.com</a>
+                                </li>
+                                <li class="w-[246px] leading-[16px] pt-[16px]">
+                                    <button>
+
+                                </li>
+                        </div>
+                    </div>
+                </section>
+                <div class="item-bottom text-center text-[#969696]">
+                    <p>© 2022 Mangcoding. All rights reserved.</p>
+                </div>
+
+        </header>
+        </footer>
+    </body>
+
+    </html>
